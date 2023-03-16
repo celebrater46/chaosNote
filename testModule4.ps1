5921a0fbb35597c6c562c8c83cd88dd1012f8c07
celebrater <tkgm45nbchk@gmail.com>
2023-03-17 07:11:28 +0900

    16th notes melody mode test succeeded

--- testModule4.ps1
+++ testModule4.ps1
@@ -3,26 +3,41 @@
 # Write-Host $obj.y
 # Write-Host $obj.width
 # Write-Host $obj.char
-
 [double] $barWidth = 441 # 80 / 128 ?
 [double] $noteHeight = 18
 [double] $shortestNote = 16 # 1 / 16, not pixel
-[double] $startX = 87
+[double] $startX = 97
 [double] $endX = 1845
 [double] $startY = 600
 [int] $maxBar = 4
 [int] $notes = $shortestNote * $maxBar
 [int] $interval = 3 # msec 
-[int] $blankRatio = 30 # X / 100
+[int] $blankRatio = 0 # X / 100
 [string] $mode = "melody" # melody / drum / bass / chord / rhythmAndChord
 
 # -1 = random, (pattern, Y)
 $drumPattern = @{
     "bars" = 1
-    "oh" = @(@(1, 0), 14)
-    "ch" = @(@(1,1,1,1,1,1,1,1), 6)
-    "snare" = @(@(0, 1, 0, 1), 2)
-    "kick" = @(@(1, 1, 1, 1), 0)
+    "oh" = @{
+        pattern = @(1, 0)
+        y = 14
+        isRandom = $FALSE
+    }
+    "ch" = @{
+        pattern = @(1,1,1,1,1,1,1,1)
+        y = 6
+        isRandom = $FALSE
+    }
+    "snare" = @{
+        pattern = @(0, 1, 0, 1)
+        y = 2
+        isRandom = $FALSE
+    }
+    "kick" = @{
+        pattern = @(1, 1, 1, 1)
+        y = 0
+        isRandom = $FALSE
+    }
 }
 
 # [int[]] $noteTypes = @(2, 4, 8, 16)
@@ -34,7 +49,20 @@ $drumPattern = @{
 [int[]] $lowerNoteWeightRatio = @(6, 3, 2, 1, 1, 1, 1, 1)
 [int[]] $scaleArray = @(0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23)
 # [int[]] $noteNumsY = & "$($PSScriptRoot)\modules\getNoteNumsY.ps1" $upperLowerRatio $upperNoteWeightRatio $lowerNoteWeightRatio $notes
-[int[]] $noteNumsY = getNoteNumsY()
+
+function getNoteNumsY(){
+    [int[]] $arr = @()
+    if($mode -eq "drum"){
+        foreach($note in $notes){
+            $arr += 0
+        }
+    } else {
+        $arr += & "$($PSScriptRoot)\modules\getNoteNumsY.ps1" $upperLowerRatio $upperNoteWeightRatio $lowerNoteWeightRatio $notes
+    }
+    return $arr
+}
+
+[int[]] $noteNumsY = getNoteNumsY
 
 # disable mouse temporary
 $Win32 = &{
@@ -62,30 +90,23 @@ $MouseMove = 0x00000001
 $MouseLeftDown = 0x0002
 $MouseLeftUp = 0x0004
 
-function getNoteNumsY(){
-    [int[]] $arr = @()
-    if($mode -eq "drum"){
-        foreach($note in $notes){
-            $arr += 0
-        }
-    } else {
-        $arr += & "$($PSScriptRoot)\modules\getNoteNumsY.ps1" $upperLowerRatio $upperNoteWeightRatio $lowerNoteWeightRatio $notes
-    }
-    return $arr
-}
-
 function createClasses(){
     $xSum = $startX
     $classes = @()
     if($mode -eq "drum"){
-        foreach($key in $drumPattern.Keys[0]){
-            $val = $drumPattern[$key];
-        }
+        # foreach($key in $drumPattern.Keys[0]){
+        #     $val = $drumPattern[$key];
+        # }
     } else {
         for($i = 0; $i -lt $notes; $i++){
             $y = $startY - ($noteHeight * $scaleArray[$noteNumsY[$i] + 1])
             # $w = $barWidth / $noteLengthArray[$i]
-            $w = $barWidth / $shortestNote
+            $w = [math]::Floor($barWidth / $shortestNote)
+            if($i % $shortestNote -eq 0){
+                # add the correction number once per 1 bar
+                $correctionNum = $shortestNote % $maxBar
+                $w += $correctionNum
+            }
             $classes += & "$($PSScriptRoot)\classes\Note.ps1" $i $xSum $y $w
             $xSum += $w
         }
@@ -103,6 +124,7 @@ function writeNote($class){
     [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($class.x, $class.y)
     Start-Sleep -m $interval
     $SendMouseEvent::mouse_event($MouseLeftDown, 0, 0, 0, 0);
+    Start-Sleep -m 1
     $SendMouseEvent::mouse_event($MouseLeftUp, 0, 0, 0, 0);
     Start-Sleep -m $interval
 }
@@ -120,12 +142,16 @@ Start-Sleep -m 3000
 
 for($i = 0; $i -lt $classArray.length; $i++){
     $random = Get-Random -Maximum 100 -Minimum 1
-    if($random -lt $blankRatio){
+    if($random -gt $blankRatio){
         writeNote $classArray[$i]
     }
+    # writeNote $classArray[$i]
 }
 
 [System.Windows.Forms.SendKeys]::SendWait(" ")
 
 Write-Host "classArray:"
-Write-Host $classArray
+# Write-Host $classArray
+foreach($class in $classArray){
+    Write-Host $class.x
+}
